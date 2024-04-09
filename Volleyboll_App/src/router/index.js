dd8325bd742e302cdc7536cda419564ba544e38d
CD47484 <47484@gymnasium.ax>
2024-04-09 11:19:56 +0300

    fix

--- Volleyboll_App/src/router/index.js
+++ Volleyboll_App/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import gruppspel from '../views/gruppspel.vue';
+import slutspel from '../views/slutspel.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,12 +8,12 @@ const router = createRouter({
     {
       path: '/gruppspel',
       name: 'gruppspel',
-      component: () => import('../views/gruppspel.vue')
+      component:gruppspel
     },
     {
       path: '/slutspel',
       name: 'slutspel',
-      component: () => import('../views/slutspel.vue')
+      component:slutspel
     }
   ]
 })
