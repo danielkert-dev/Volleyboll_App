1546fa552549c78084c29452f21d19986558481c
CD47484 <47484@gymnasium.ax>
2024-04-09 11:06:52 +0300

    liten fix

--- Volleyboll_App/src/views/slutspel.vue
+++ Volleyboll_App/src/views/slutspel.vue
@@ -124,6 +124,14 @@ export default {
     document.removeEventListener("click", this.closeDropdownsOnClickOutside);
   }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const plusImg = document.querySelector('.plus-img');
+    const box = document.querySelector('.box');
+
+    plusImg.addEventListener('click', function() {
+        box.style.display = (box.style.display === 'none') ? 'block' : 'none';
+    });
+});
 </script>
 
 <template>
@@ -144,14 +152,13 @@ export default {
   <img class="phone-img" src="https://cdn-icons-png.freepik.com/512/68/68737.png" alt="turn the phone">
   </div>
 
+  <div class="plus">
+    <img class="plus-img" src="../assets/plus.png">
+  </div>
   <div class="box">
     <p class="boxtext">Most points: IT21</p>
     <p class="boxtext">Best W/L: IT21</p>
     <p class="boxtext">Price: Pizza</p>
-  </div>
-
-  <div class="phone-container">
-  <img class="phone-img" src="https://cdn-icons-png.freepik.com/512/68/68737.png" alt="turn the phone">
   </div>
 
 </template>
@@ -181,15 +188,24 @@ export default {
   color: white; /* Assuming you want the text to be white for visibility */
 }
 
+.plus{
+  position:fixed!important;
+  bottom:5px;
+  right:5px;
+  height:48px;
+  width:48px;
+}
+
 .box {
-  position:absolute;
+  position:fixed!important;
   border: solid black;
   border-radius:30px;
   text-align: center;
-  bottom:0%;
-  right:0%;
-  margin:5%;
+  bottom:20px;
+  right:50px;
+  display:none;
 }
+
 .boxtext {
   font-size:1.2em;
   padding:3%;
